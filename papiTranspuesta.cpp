16c2a68c6aa026829ae8c739d635ad73e44b0496
Oscar Peñuela R <odpenuelar@unal.edu.co>
2021-05-15 03:30:31 -0500

    cambio transpuesta

--- papiTranspuesta.cpp
+++ papiTranspuesta.cpp
@@ -14,9 +14,9 @@ int main(int argc, char **argv)
   
 
     //Obtener argumentos    
-    const int N = std::atoi(argv[1]);
+    const int a = std::atoi(argv[1]);
    
-  
+   for(int N=0; N<a; ++N ){
        float real_time=0.0, proc_time=0.0,mflops=0.0;
         long long flpops=0.0;
         float ireal_time=0.0, iproc_time=0.0, imflops=0.0;
@@ -26,7 +26,6 @@ int main(int argc, char **argv)
         double A[N][N];
         double AT[N][N];
         srand(time(NULL));
-
         // inicializar matrices
         for (int ii = 0; ii < N; ++ii) {
         for (int jj = 0; jj < N; ++jj) {
@@ -61,7 +60,16 @@ int main(int argc, char **argv)
         }
         printf("Real_time: %f Proc_time: %f Total flpops: %lld MFLOPS: %f\n",
         real_time, proc_time,flpops,mflops);
-    
-    
+        
+        long suma;
+
+         for (int ii = 0; ii < N; ++ii) {
+            for (int jj = 0; jj < N; ++jj) {
+                suma +=A[jj][ii]; 
+            
+            }
+        }
+
+    }
     return 0;
 }
