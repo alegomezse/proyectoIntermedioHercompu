115ff5d3a72521f3754b8ea91d6145a2d6c15964
alegomezse <79580651+alegomezse@users.noreply.github.com>
2021-05-15 00:51:57 -0500

    Update papiMultiplicacion.cpp

--- papiMultiplicacion.cpp
+++ papiMultiplicacion.cpp
@@ -28,7 +28,7 @@ printf("Your platform may not support floating point operation event.\n");
 printf("retval: %d\n", retval);
 exit(1);
 }
-code_to_be_measured(A, AT, N);
+code_to_be_measured(A,B);
 if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)
 {
 printf("retval: %d\n", retval);
@@ -51,7 +51,6 @@ std::cout << "Multiplicación A*B = " << std::endl;
     int sizeCB = B[0].size();
     int suma = 0;
     std::vector<std::vector<double>> R(sizeA, std::vector<double> (sizeCB, 0));
-    R = fillMatrix(R);
     for(int i = 0; i < sizeA;i++){
         for(int j = 0;j < sizeCB;j++){
             for(int k = 0;k < n;k++){
