da46b8bd6faeae805cc2ad35cd06dc949239ed53
Estudiante19 Pr_wfoquendo <woquendo_St19@hercules4.biocomputo.unal.edu.co>
2021-05-15 03:11:32 -0500

    se añadieron los primerios archivos con papi que calculan la cantidad de flops gastados

--- papiMultiplicacion.cpp
+++ papiMultiplicacion.cpp
@@ -9,10 +9,17 @@ int main(int argc, char **argv)
 {
 const int N = std::atoi(argv[1]);
 // Matrix declaration : Modeled as  nD vectors
+for(int ii= 0; ii< N ; ii++){
+// initialize matrices 
+std::vector<std::vector<double>> A( N , std::vector<double> (N, 0));
+std::vector<std::vector<double>> B( N , std::vector<double> (N, 0));
 
-// initialize matrices
- std::vector<std::vector<double>> A = {{1.5,2.0},{2.5,4.0}};
- std::vector<std::vector<double>> B = {{5.0,6.0},{7.44,8.03}};
+ for(int i=0;i < ii;i++){
+  for(int j=0;j < ii;j++){
+ 	 A[i][j] = rand() % 100;
+ 	 B[i][j] = rand() % 100;
+      }
+}
 // PAPI vars
 float real_time, proc_time,mflops;
 long long flpops;
@@ -38,13 +45,12 @@ printf("Real_time: %f Proc_time: %f Total flpops: %lld MFLOPS: %f\n",
 real_time, proc_time,flpops,mflops);
 // Do something here, like computing the average of the resulting matrix, to avoid the optimizer deleting the code
 printf("%.15e\n", r);
-
+}
 return 0;
 }
 double code_to_be_measured(std::vector<std::vector<double>> A,std::vector<std::vector<double>> B)
 {
 // simple matrix multiplication
-std::cout << "Multiplicación A*B = " << std::endl;
     int sizeA  = A.size();
     int n = B.size();
     int sizeCB = B[0].size();
@@ -55,9 +61,7 @@ std::cout << "Multiplicación A*B = " << std::endl;
             for(int k = 0;k < n;k++){
                 R[i][j] += A[i][k]*B[k][j];  
                 suma += A[i][k]*B[k][j];
-                printf("i = ",i);
-                printf("j = ",j);
-                printf("k = ",k);
+               
             }
         }
     }
