1caa1f980a89ec7224ee6077177f29bcd0fe4cc9
Oscar Peñuela R <odpenuelar@unal.edu.co>
2021-05-15 13:39:49 -0500

    modificación datos armadillo en una sola linea

--- papiTranspuestaArmadillo.cpp
+++ papiTranspuestaArmadillo.cpp
@@ -8,41 +8,41 @@ using namespace arma;
 mat code_to_be_measured(const mat& m);
 int main(int argc, char **argv)
 {
-const int N = std::atoi(argv[1]);
-for(int ii = 0; ii < N; ii++){
+  const int N = std::atoi(argv[1]);
+  for(int ii = 0; ii < N; ii++){
 
-// Matrix declaration : Modeled as  nD vectors
+    // Matrix declaration : Modeled as  nD vectors
 
-// initialize matrices
-  mat A(ii, ii, fill::randu);
-// PAPI vars
-float real_time, proc_time,mflops;
-long long flpops;
-float ireal_time, iproc_time, imflops;
-long long iflpops;
-int retval;
-// PERFOMANCE MEASURE
-// start PAPI counters
-if((retval=PAPI_flops_rate(PAPI_FP_OPS,&ireal_time,&iproc_time,&iflpops,&imflops)) < PAPI_OK)
-{
-printf("Could not initialise PAPI_flops \n");
-printf("Your platform may not support floating point operation event.\n");
-printf("retval: %d\n", retval);
-exit(1);
-}
-mat b = code_to_be_measured(A);
-double suma = accu(b);
-if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)
-{
-printf("retval: %d\n", retval);
-exit(1);
-}
-printf("Real_time: %f Proc_time: %f Total flpops: %lld MFLOPS: %f\n",
-real_time, proc_time,flpops,mflops);
-// Do something here, like computing the average of the resulting matrix, to avoid the optimizer deleting the code
-printf("%.15e\n", b);
-}
-return 0;
+    // initialize matrices
+      mat A(ii, ii, fill::randu);
+    // PAPI vars
+    float real_time, proc_time,mflops;
+    long long flpops;
+    float ireal_time, iproc_time, imflops;
+    long long iflpops;
+    int retval;
+    // PERFOMANCE MEASURE
+    // start PAPI counters
+    if((retval=PAPI_flops_rate(PAPI_FP_OPS,&ireal_time,&iproc_time,&iflpops,&imflops)) < PAPI_OK)
+    {
+    printf("Could not initialise PAPI_flops \n");
+    printf("Your platform may not support floating point operation event.\n");
+    printf("retval: %d\n", retval);
+    exit(1);
+    }
+    mat b = code_to_be_measured(A);
+    double suma = accu(b);
+    if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)
+    {
+      printf("retval: %d\n", retval);
+      exit(1);
+    }
+    printf("Dimensíon_Matriz: %d Real_time: %f Proc_time: %f Total flpops: %lld MFLOPS: %f\n",
+    ii,real_time, proc_time,flpops,mflops);
+    // Do something here, like computing the average of the resulting matrix, to avoid the optimizer deleting the code
+    //printf("%.15e\n", b);
+  }
+  return 0;
 }
 
 mat code_to_be_measured(const mat& m)
