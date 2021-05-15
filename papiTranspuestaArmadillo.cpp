6a11a355e4553bbfa10fb85a9af3c508f650bac4
Oscar Peñuela R <odpenuelar@unal.edu.co>
2021-05-15 14:23:29 -0500

    se modifico datos de salida armadillo para que estuvieran en una sola fial

--- papiTranspuestaArmadillo.cpp
+++ papiTranspuestaArmadillo.cpp
@@ -31,16 +31,18 @@ int main(int argc, char **argv)
     exit(1);
     }
     mat b = code_to_be_measured(A);
-    double suma = accu(b);
+   
     if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)
     {
       printf("retval: %d\n", retval);
       exit(1);
     }
+    double suma = accu(b);
     printf("Dimensíon_Matriz: %d Real_time: %f Proc_time: %f Total flpops: %lld MFLOPS: %f\n",
     ii,real_time, proc_time,flpops,mflops);
     // Do something here, like computing the average of the resulting matrix, to avoid the optimizer deleting the code
     //printf("%.15e\n", b);
+     
   }
   return 0;
 }
--- papiTranspuestaEigen.cpp
+++ papiTranspuestaEigen.cpp
@@ -35,10 +35,11 @@ int main(int argc, char **argv)
       printf("retval: %d\n", retval);
       exit(1);
     }
+      r.sum();
     printf("Dimensíon_Matriz: %d Real_time: %f Proc_time: %f Total flpops: %lld MFLOPS: %f\n",
     ii,real_time, proc_time,flpops,mflops);
     // Do something here, like computing the average of the resulting matrix, to avoid the optimizer deleting the code
-    r.sum();
+  
     //printf("%.15e\n", r.sum());
   }
   return 0;
